3b5a5ef6a65206d8cadb6543a768ebf9b19876da
unknown <andrewphirsov@gmail.com>
2018-12-19 10:01:48 +0300

    Test

---
+++ on-box/configs/base/One.py
@@ -0,0 +1,6 @@
+mylist=["Vampire Daemon","Vampire Angel","Saint Santa","Blessed Fish"]
+print ("The current list is:")
+print (mylist)
+item=input("Add new piece:")
+mylist.append(item)
+print (mylist)
