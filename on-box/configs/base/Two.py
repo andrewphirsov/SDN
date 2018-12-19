533f8851137206e5f6e222979287d4c2a7f37d24
unknown <andrewphirsov@gmail.com>
2018-12-19 13:47:28 +0300

    t

--- on-box/configs/base/Two.py
+++ on-box/configs/base/Two.py
@@ -17,7 +17,7 @@ output = net_connect.send_config_set (config_commands)
 print (output)
 
 for n in range (2,20):
-    print ('Creating VLAN' + str(n))
+    #print ('Creating VLAN' + str(n))
     config_commands = ['vlan ' + str (n), "name " + str(n)]
     #output = net_connect.send_config_set (config_commands)
     net_connect.send_config_set (config_commands)
