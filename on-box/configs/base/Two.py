5e86a653616c2bcdea6a30db9bfe45c1faa67f50
unknown <andrewphirsov@gmail.com>
2018-12-19 13:33:26 +0300

    Added VLANs to switch

--- on-box/configs/base/Two.py
+++ on-box/configs/base/Two.py
@@ -15,7 +15,12 @@ print (output)
 config_commands = ["hostname PY_SWITCH","int loop 0","ip address 1.1.1.1 255.255.255.0"]
 output = net_connect.send_config_set (config_commands)
 print (output)
-    
+
+for n in range (2,20)
+    print ('Creating VLAN' + str(n))
+    config_commands = ['vlan' + str (n), "name" + str(n)]
+    output = net_connect.send_config_set (config_commands)
+    print (output)
 
 #test
 #output = net_connect.send_config_set("int loop 0", "ip address 1.1.1.1 255.255.255.0")
