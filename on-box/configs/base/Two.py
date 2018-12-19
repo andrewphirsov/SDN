99959b5c328d692f97801a163c1a85157a757490
unknown <andrewphirsov@gmail.com>
2018-12-19 09:58:56 +0300

    f

---
+++ on-box/configs/base/Two.py
@@ -0,0 +1,21 @@
+from netmiko import ConnectHandler
+
+
+
+ios_l2 = {
+    'device_type': 'cisco_ios',
+    'ip': '10.100.100.100',
+    'username':'admin',
+    'password':'cisco'
+}
+
+net_connect = ConnectHandler(**ios_l2)
+output = net_connect.send_command ('show ip int brief')
+print (output)
+config_commands = ["hostname PY_SWITCH","int loop 0","ip address 1.1.1.1 255.255.255.0"]
+output = net_connect.send_config_set (config_commands)
+print (output)
+    
+
+
+#output = net_connect.send_config_set("int loop 0", "ip address 1.1.1.1 255.255.255.0")
